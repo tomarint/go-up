01791e275622bc21a5df803f5d307adf92ce4367
tomarin <tomarint@gmail.com>
2023-04-14 03:24:56 +0900

    Adjust width of the list

--- src/background/background.ts
+++ src/background/background.ts
@@ -5,8 +5,26 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
       const urlList = [currentUrl];
       let url = currentUrl;
       if (url != null) {
-        while (url.lastIndexOf("/") > 7) {
-          url = url.substring(0, url.lastIndexOf("/"));
+        // Check if url contains # or ? and remove them
+        if (url.indexOf("#") > 0) {
+          url = url.substring(0, url.indexOf("#"));
+          urlList.push(url);
+        }
+        if (url.indexOf("?") > 0) {
+          url = url.substring(0, url.indexOf("?"));
+          urlList.push(url);
+        }
+        // Remove last slash of url
+        if (url.endsWith("/") && !url.endsWith("//")) {
+          url = url.substring(0, url.length - 1);
+        }
+        // Remove last directory of url
+        let idx = 0;
+        while ((idx = url.lastIndexOf("/")) > 0) {
+          if (idx > 0 && url.substring(idx - 1, idx) === "/") {
+            break;
+          }
+          url = url.substring(0, idx);
           urlList.push(url);
         }
         sendResponse({ urls: urlList });
--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -4,7 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main") as HTMLDivElement;
     urlList.forEach((url: string) => {
       const div = document.createElement("div");
-      div.style.margin = "0.5rem";
+      div.style.display = "flex";
+      div.style.margin = "0.0rem";
+      div.style.padding = "0.5rem";
+      // div.style.overflowWrap = "anywhere";
+      // div.style.whiteSpace = "pre";
+      // div.style.wordBreak = "break-all";
+      // div.style.whiteSpace = "pre-wrap";
+      // div.style.width = "max(100%,max-content)";
+      // div.style.width = "max-content";
+      div.style.width = "100%";
+      // div.style.wordBreak = "break-all";
+      div.style.borderBottom = "0.1rem solid #eee";
+
+      // if clicked, open url in current tab
+      div.onclick = (event: MouseEvent) => {
+        event.preventDefault();
+        chrome.tabs.update({ url: url }, () => {
+          window.close();
+        });
+      };
+      div.style.cursor = "pointer";
+      div.addEventListener("mouseover", (event: MouseEvent) => {
+        div.style.backgroundColor = "#eee";
+      });
+      div.addEventListener("mouseout", (event: MouseEvent) => {
+        div.style.backgroundColor = "#fff";
+      });      
       const link = document.createElement("a");
       link.href = url;
       link.innerText = url;
