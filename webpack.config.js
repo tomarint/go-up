b0d5701d1cc143a90a9d0f3ed23e8c06c4ee28e6
tomarin <tomarint@gmail.com>
2023-08-06 22:05:17 +0900

    Version 1.1.0

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ const createBrowserConfig = (browser) => {
             },
             transform: async (content) => {
               const data = {
-                version: "1.0.1",
+                version: "1.1.0",
                 browser: browser,
               };
 
